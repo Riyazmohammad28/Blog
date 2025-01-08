821f0cdbda29da08f6b2ea8c69266c9921d0c580
Riyazmohammad28 <md.quayyum786786ff@gmail.com>
2025-01-08 13:46:45 +0530

    Update index.js

--- Blog-Website/server/index.js
+++ Blog-Website/server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors('https://blog-cang.vercel.app'));
+app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
